178c9f8034246621350be4d73deacdb46bb24d7d
samanthagwynetha <s.arsua.541584@umindanao.edu.ph>
2026-06-26 19:11:24 +0800

    feat: implement token bucket rate limiter service

--- src/main/java/com/finlock/finlock/common/ratelimit/RateLimiterService.java
+++ src/main/java/com/finlock/finlock/common/ratelimit/RateLimiterService.java
@@ -1,4 +1,26 @@
 package com.finlock.finlock.common.ratelimit;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Service
 public class RateLimiterService {
+    private final ConcurrentMap<String, Bucket> buckets = new ConcurrentHashMap<>();
+    public Bucket resolveBucket(String key, int capacity, Duration refillPeriod){
+        return buckets.computeIfAbsent(key, k -> createNewBucket(capacity, refillPeriod));
+    }
+    private Bucket createNewBucket(int capacity, Duration refillPeriod) {
+        Bandwidth limit = Bandwidth.classic(capacity, Refill.greedy(capacity, refillPeriod));
+        return Bucket.builder().addLimit(limit).build();
+    }
+    public boolean tryConsume(String key, int capacity, Duration refillPeriod){
+        Bucket bucket = resolveBucket(key, capacity, refillPeriod);
+        return bucket.tryConsume(1);
+    }
 }
