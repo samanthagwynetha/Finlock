4dee832c8a98871a4f2cf4565dcb0c2e4f1b7620
samanthagwynetha <s.arsua.541584@umindanao.edu.ph>
2026-07-03 18:59:26 +0800

    feat: add Dead Letter Queue consumer for failed transaction events

--- src/main/java/com/finlock/finlock/transaction/event/DeadLetterConsumer.java
+++ src/main/java/com/finlock/finlock/transaction/event/DeadLetterConsumer.java
@@ -1,4 +1,24 @@
 package com.finlock.finlock.transaction.event;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class DeadLetterConsumer {
+    @KafkaListener(
+            topics = TransactionEventProducer.TOPIC + "-dlt",
+            groupId = "finlock-dlq-group"
+    )
+    public void consumeDeadLetter(
+            String message,
+            @Header(value = "kafka_dlt-exception-message", required = false)
+            String exceptionMessage) {
+
+        log.error("[DEAD LETTER] Failed message recieved.");
+        log.error("[DEAD LETTER] Reason: {}", exceptionMessage);
+        log.error("[DEAD LETTER] Raw message: {}", message);
+    }
 }
