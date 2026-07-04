e07bd1002b7dded3c6cc35309c2146de2608bdff
samanthagwynetha <s.arsua.541584@umindanao.edu.ph>
2026-07-04 12:41:41 +0800

    chore: add Spring Boot application context test

---
+++ src/test/java/com/finlock/finlock/FinlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finlock.finlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
